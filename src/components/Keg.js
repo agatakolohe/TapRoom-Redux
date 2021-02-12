0cedcac4e2ab7b14bd6573e556035bcd6144a991
agatakolohe <agatakolohe@gmail.com>
2021-02-12 10:09:00 -0800

    create keg component

---
+++ src/components/Keg.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenKegClicked(props.id)}>
+        <h2>Name: {props.name}</h2>
+        <h3>Brand: {props.brand}</h3>
+        <h4>ABV: {props.abv}</h4>
+        <p>${props.price}</p>
+        <h4>
+          Pints:{" "}
+          {props.pintQuantity > 0 ? (
+            props.pintQuantity
+          ) : (
+            <div style={{ color: "red" }}> Kicked it! (Out of Stock)</div>
+          )}
+        </h4>
+      </div>
+      <button
+        class="btn btn-warning"
+        onClick={() => props.sellButton(props.id)}
+      >
+        One Beer
+      </button>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  abv: PropTypes.string,
+  pintQuantity: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func,
+  sellButton: PropTypes.func,
+};
+
+export default Keg;
