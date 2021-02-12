32e73e5070c739eb64fe7c3c8773abacbd47d0db
agatakolohe <agatakolohe@gmail.com>
2021-02-12 11:02:50 -0800

    create kegdetail component

---
+++ src/components/KegDetail.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetail(props) {
+  const { keg } = props;
+  return (
+    <React.Fragment>
+      <h1>Keg Details:</h1>
+      <h2>Name: {keg.name}</h2>
+      <h3>Brand: {keg.brand}</h3>
+      <h4>ABV: {keg.abv}</h4>
+      <p>Price: {keg.price}</p>
+      <h5>
+        Pints:{" "}
+        {keg.pintQuantity > 0 ? (
+          keg.pintQuantity
+        ) : (
+          <div style={{ color: "red" }}>
+            Can I kick it? (Yes, you can!) (Out of Stock)
+          </div>
+        )}
+      </h5>
+      <hr />
+      <button
+        class="btn btn-danger"
+        onClick={() => props.onClickingDelete(keg.id)}
+      >
+        Delete Keg
+      </button>
+    </React.Fragment>
+  );
+}
+KegDetail.propTypres = {
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+};
+
+export default KegDetail;
