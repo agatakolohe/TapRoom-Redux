e390121d147ae73564f28a9509de572381bcde47
agatakolohe <agatakolohe@gmail.com>
2021-02-19 08:34:33 -0800

    test kegListReducer ADDKEG action, pass

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, abv, pintQuantity, id } = action;
+  switch (action.type) {
+    case "ADD_KEG":
+      return Object.assign({}, state, {
+        [id]: {
+          name,
+          brand,
+          price,
+          abv,
+          pintQuantity,
+          id,
+        },
+      });
+    default:
+      return state;
+  }
 };
