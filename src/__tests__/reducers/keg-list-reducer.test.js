237944595586fe216cee1b1ff3b3cb485a67b215
agatakolohe <agatakolohe@gmail.com>
2021-02-19 08:32:08 -0800

    test kegListReducer ADDKEG action, good fail

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,40 @@
 import kegListReducer from "../../reducers/keg-list-reducer";
 
 describe("kegListReducer", () => {
+  let action;
+  const kegData = {
+    name: "Hoptimum",
+    brand: "Sierra Nevada",
+    price: "30",
+    abv: "10",
+    pintQuantity: "100",
+    id: 1,
+  };
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+  test("Should successfully add new keg data to masterKegList", () => {
+    const { name, brand, price, abv, pintQuantity, id } = kegData;
+    action = {
+      type: "ADD_KEG",
+      name,
+      brand,
+      price,
+      abv,
+      pintQuantity,
+      id,
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name,
+        brand,
+        price,
+        abv,
+        pintQuantity,
+        id,
+      },
+    });
   });
 });
