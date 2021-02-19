f25d5f8f1c99ed57562b58780e6defc3491d65cc
agatakolohe <agatakolohe@gmail.com>
2021-02-19 08:10:40 -0800

    clone taproom, rm remote, create tests & reducers dir

---
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -0,0 +1,5 @@
+// import kegListReducer from '../../reducers/keg-list-reducer';
+
+// describe('kegListReducer', () => {
+//   test('Should return default state if there is no action type passed into the reducer')
+// })
