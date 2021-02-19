3a5a6a924885e02fee100382b4b129d8d8c3815e
agatakolohe <agatakolohe@gmail.com>
2021-02-19 09:30:42 -0800

    rootReducer smoketest for kegListReducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,10 @@
 import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import kegListReducer from "../../reducers/keg-list-reducer";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
+import editingReducer from "../../reducers/editing-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test("Should return default state if no action type is recognized", () => {
@@ -7,5 +13,11 @@ describe("rootReducer", () => {
       formVisibleOnPage: false,
       editing: false,
     });
+  });
+
+  test("Check that inital state of kegListReducer matches root reducer", () => {
+    expect(store.getState().masterKegList).toEqual(
+      kegListReducer(undefined, { type: null })
+    );
   });
 });
