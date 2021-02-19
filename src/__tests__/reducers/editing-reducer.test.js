21fd38bf3b38e8fe5f4c2cc15ade150a26532e36
agatakolohe <agatakolohe@gmail.com>
2021-02-19 08:57:21 -0800

    test editingReducer TOGGLEEDIT action, good fail

--- src/__tests__/reducers/editing-reducer.test.js
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -4,4 +4,8 @@ describe("editingReducer", () => {
   test("Should return default state if no action type is recognized", () => {
     expect(editingReducer(false, { type: null })).toEqual(false);
   });
+
+  test("Should toggle editing state to true", () => {
+    expect(editingReducer(false, { type: "TOGGLE_EDIT" })).toEqual(true);
+  });
 });
