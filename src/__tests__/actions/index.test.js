519586487eae79294cfe68d6f23a99962ce057bd
agatakolohe <agatakolohe@gmail.com>
2021-02-19 11:36:45 -0800

    test action toggleForm(), good fail

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -25,4 +25,8 @@ describe("TapRoom actions", () => {
   it("deleteKeg should create DELETE_KEG action", () => {
     expect(actions.deleteKeg(1)).toEqual({ type: "DELETE_KEG", id: 1 });
   });
+
+  it("toggleForm should create TOGGLE_FORM action", () => {
+    expect(actions.toggleForm()).toEqual({ type: "TOGGLE_FORM" });
+  });
 });
