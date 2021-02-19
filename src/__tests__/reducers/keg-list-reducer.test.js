3ae0c20ab937ce425541962378712f0ebe011f72
agatakolohe <agatakolohe@gmail.com>
2021-02-19 08:24:26 -0800

    test kegListReducer, good fail

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,5 +1,7 @@
-// import kegListReducer from '../../reducers/keg-list-reducer';
+import kegListReducer from "../../reducers/keg-list-reducer";
 
-// describe('kegListReducer', () => {
-//   test('Should return default state if there is no action type passed into the reducer')
-// })
+describe("kegListReducer", () => {
+  test("Should return default state if there is no action type passed into the reducer", () => {
+    expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+});
