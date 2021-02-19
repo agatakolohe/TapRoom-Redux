248e633eb70d174dd160e9d6cf531587cbc0f499
agatakolohe <agatakolohe@gmail.com>
2021-02-19 09:24:25 -0800

    test rootReducer default state, good fail

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// import formVisibleReducer from "./form-visible-reducer";
+// import kegListReducer from "./keg-list-reducer";
+// import editingReducer from "./editing-reducer";
+// import { combineReducers } from "redux";
+
+// const rootReducer = combineReducers({
+//   masterKegList: kegListReducer,
+//   formVisibleOnPage: formVisibleReducer,
+//   editing: editingReducer,
+// });
+
+// export default rootReducer;
